009a3f06528d3fb78b7190c4b43732423419656a
Földi Tamás <tfoldi@xsi.hu>
2022-12-22 07:41:03 +0100

    Add license

--- deployment/kubernetes/__main__.py
+++ deployment/kubernetes/__main__.py
@@ -1,4 +1,26 @@
-"""A Kubernetes Python Pulumi program"""
+"""
+MIT License
+
+Copyright (c) 2022 HCLTech
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 import pulumi
 from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
@@ -7,48 +29,17 @@ from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpe
 from pulumi_kubernetes.helm.v3 import Release, ReleaseArgs, RepositoryOptsArgs
 from pulumi_kubernetes.core.v1 import Namespace, Service
 from pulumi_kubernetes.storage.v1 import StorageClass, StorageClassArgs
+import storage
 
 config = pulumi.Config()
+components = config.require_object("components")
 stack = pulumi.get_stack()
 org = config.require("org")
+resources = {}
+
 
 aws_stack_ref = pulumi.StackReference(f"{org}/antares-idl-aws/{stack}")
+resources["aws_stack_ref"] = aws_stack_ref
 
-aws_efs_csi_driver = Release(
-    "aws-efs-csi-driver",
-    ReleaseArgs(
-        chart="aws-efs-csi-driver",
-        name="aws-efs-csi-driver",
-        repository_opts=RepositoryOptsArgs(
-            repo="https://kubernetes-sigs.github.io/aws-efs-csi-driver/",
-        ),
-        namespace="kube-system",
-        values={
-            "image": {
-                "repository": "602401143452.dkr.ecr.eu-central-1.amazonaws.com/eks/aws-efs-csi-driver",
-            },
-            "controller": {
-                "serviceAccount": {
-                    "create": False,
-                    "name": "efs-csi-controller-sa",
-                }
-            }
-        },
-    ),
-)
-
-status = aws_efs_csi_driver.status
-pulumi.export("aws-efs-csi-driver", aws_efs_csi_driver.status.name)
-
-efs_storage_class = StorageClass("efs-sc",
-                                 metadata=ObjectMetaArgs(
-                                     name= "efs-sc",
-                                 ),
-                                 provisioner="efs.csi.aws.com",
-                                 parameters={
-                                     "provisioningMode": "efs-ap",
-                                     "fileSystemId": aws_stack_ref.get_output("url"),
-                                     "directoryPerms": "700",
-                                     "basePath": "/eks_dynamic",
-                                 },
-                                 )
+if components["efs"]:
+    storage.configure_efs_storage(resources)
